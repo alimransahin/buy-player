23823cc48ab050742477d227c93d54d325fdda9a
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-08-23 20:36:51 +0600

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const playerList=document.getElementById('player-list');
-document.getElementById('player').addEventListener('click', function(event){
-    const li=document.createElement('li');
+const playerList = document.getElementById('player-list');
+document.getElementById('player').addEventListener('click', function (event) {
+    const li = document.createElement('li');
     li.innerText = event.target.value;
-    const totalPlayers=playerList.childElementCount;
-    if(totalPlayers<5){
-        if(li.innerText!='undefined'){
+    const totalPlayers = playerList.childElementCount;
+    if (totalPlayers < 5) {
+        if (li.innerText != 'undefined') {
             playerList.appendChild(li);
             event.target.style.backgroundColor = "gray";
             event.target.disabled = true;
         }
     }
-    else{
+    else {
         alert('You have already selected 5 players');
     }
 })
-function getElementValueById(elementId){
-    const element=document.getElementById(elementId);
-    const elementValueString=element.value;
-    const elementValue=parseInt(elementValueString);
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.value;
+    const elementValue = parseInt(elementValueString);
     return elementValue;
 }
 function getElementTextById(elementId) {
@@ -26,28 +26,28 @@ function getElementTextById(elementId) {
     const elementValue = parseInt(elementValueString);
     return elementValue;
 }
-function setElementTextById(elementId, value){
-    const element=document.getElementById(elementId);
-    element.innerText=value;
+function setElementTextById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
-document.getElementById('player-budget').addEventListener('click', function(){
-    const perPlayer=getElementValueById('per-player');
+document.getElementById('player-budget').addEventListener('click', function () {
+    const perPlayer = getElementValueById('per-player');
     const totalPlayerNo = playerList.childElementCount;
-    if(Number.isInteger(perPlayer)&&perPlayer>=0){
-        const totalPlayerBudget=perPlayer*totalPlayerNo;
-        setElementTextById('total-player-budget',totalPlayerBudget)
+    if (Number.isInteger(perPlayer) && perPlayer >= 0) {
+        const totalPlayerBudget = perPlayer * totalPlayerNo;
+        setElementTextById('total-player-budget', totalPlayerBudget)
     }
-    else{
+    else {
         alert("Please Insert Valid Per Player Budget");
     }
 })
 
-document.getElementById('calculate').addEventListener('click',function(){
+document.getElementById('calculate').addEventListener('click', function () {
     const playerBudget = getElementTextById('total-player-budget');
-    const manager=getElementValueById('manager');
-    const coach=getElementValueById('coach');
+    const manager = getElementValueById('manager');
+    const coach = getElementValueById('coach');
     if (Number.isInteger(manager) && manager >= 0 && Number.isInteger(coach) && coach >= 0) {
-        const totalCost =playerBudget + manager + coach;
+        const totalCost = playerBudget + manager + coach;
         setElementTextById('total', totalCost);
     }
     else {
